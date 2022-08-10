254839a0ec499339db0f1175553dbf1ac193686b
Aliensgamer <juan.trujillo01@correo.usa.edu.co>
2022-08-09 19:53:09 -0500

    Punto 1

---
+++ taller01/punto1.cpp
@@ -0,0 +1,22 @@
+/***************************
+ * Fecha: 10-08-2022
+ * Autor: Santiago Gutiérrez
+ * Materia: HPC-2
+ * Tema: Primer taller de ejercicios
+ * Programa: Punto 1
+****************************/
+
+#include <iostream>
+
+int main() {
+    int x = 1, y = 1;
+    
+    if (x = y * 5)
+        x = 0;
+    else
+        x = -1;
+    
+    std::cout << "x = " << x << std::endl;
+
+    return 0;
+}
