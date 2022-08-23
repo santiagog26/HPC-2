b4ca460b56b18817f3e62b696072b84cbc69d2f7
Aliensgamer <juan.trujillo01@correo.usa.edu.co>
2022-08-22 20:34:33 -0500

    Se crea proyecto para desarrollo de taller

---
+++ taller02/Debug/CMakeFiles/MatrixEquations.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MatrixEquations.dir/main.cpp.o"
+  "MatrixEquations"
+  "MatrixEquations.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MatrixEquations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ taller02/MatrixEquations/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+
+int main()
+{
+    // Declaration variables
+    int exercise = 0;
+
+    // Input for the number of the exercise
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Please write the exercise you want to resolve: " << std::endl;
+    std:q:cin >> exercise;
+
+    switch (exercise) {
+        case 1:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        case 2:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        case 3:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        case 4:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        case 5:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        case 6:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        case 7:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        case 8:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        case 9:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        case 10:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        case 11:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        case 12:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        case 13:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        case 14:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        case 15:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        case 16:
+            std::cout << "Exercise " << exercise << std::endl;
+            break;
+        default:
+            std::cout << "Try another number between 1 and 16!" << std::endl;
+    }
+
+    return 0;
+}
